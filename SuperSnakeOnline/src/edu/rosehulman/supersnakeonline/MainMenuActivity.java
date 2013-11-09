8d44a3468d4b4e89b7eb98f5f4f2c615d287c8b4
Joe Lee <leed1@rose-hulman.edu>
2013-11-09 17:01:54 -0500

    background music

--- SuperSnakeOnline/src/edu/rosehulman/supersnakeonline/MainMenuActivity.java
+++ SuperSnakeOnline/src/edu/rosehulman/supersnakeonline/MainMenuActivity.java
@@ -1,8 +1,13 @@
 package edu.rosehulman.supersnakeonline;
 
+import edu.rosehulman.supersnakeonline.SnakeMusic.ServiceBinder;
 import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -20,12 +25,49 @@ public class MainMenuActivity extends Activity implements OnClickListener {
 	public static int COLOR_GREEN = 0;
 	public static int COLOR_YELLOW = 1;
 	public static int COLOR_RED = 2;
+	private boolean mIsBound = false;
+	private SnakeMusic mServ;
+	
+	private ServiceConnection conn = new ServiceConnection() {
+		
+		@Override
+		public void onServiceDisconnected(ComponentName name) {
+			mServ = null;
+		}
+		
+		@Override
+		public void onServiceConnected(ComponentName name, IBinder service) {
+			ServiceBinder binder = (ServiceBinder)service;
+			mServ = binder.getService();
+		}
+	};
+	
+	private void doBindService(Intent music){
+		music.setClass(this, SnakeMusic.class);
+ 		bindService(music, conn, Context.BIND_AUTO_CREATE);
+		mIsBound = true;
+	}
+
+	private void doUnbindService()
+	{
+		if(mIsBound)
+		{
+			unbindService(conn);
+      		mIsBound = false;
+		}
+	}
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main_menu);
 		
+		// background music
+		Intent music = new Intent();
+		doBindService(music);
+		startService(music);
+		
+		// button handler
 		((Button)findViewById(R.id.one_player_button)).setOnClickListener(this);
 		((Button)findViewById(R.id.two_player_button)).setOnClickListener(this);
 		((Button)findViewById(R.id.settings_button)).setOnClickListener(this);
@@ -67,5 +109,11 @@ public class MainMenuActivity extends Activity implements OnClickListener {
 			break;
 		}
 	}
-
+	
+	@Override
+	protected void onDestroy() {
+		mServ.stopMusic();
+		doUnbindService();
+		super.onDestroy();
+	}
 }
